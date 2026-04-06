b450a290d1c705ad65e496294e01dc322a5941ca
Elivelton Gomes <brogomes153@gmail.com>
2026-04-06 12:43:06 -0300

    Adicionado o envio dos dados para nuvem

--- collect.py
+++ collect.py
@@ -4,13 +4,6 @@ pd.set_option('display.max_columns', None)
 import fastf1
 import time
 import argparse
-<<<<<<< HEAD
-=======
-
-#%%
-#df = pd.read_parquet("data/2019_01_R.parquet")
-#df
->>>>>>> 8641fe4 (Adicionado o envio dos dados para nuvem)
 #%%
 
 class CollectResults:
@@ -26,16 +19,6 @@ class CollectResults:
             return pd.DataFrame()
         
         session._load_drivers_results()
-<<<<<<< HEAD
-
-        df = session.results
-        df["Mode"] = mode
-
-        return df
-    
-    def save_data(self,df,year,gp,mode):
-        df.to_parquet(f"data/{year}_{gp:02}_{mode}.parquet")
-=======
         df = session.results
 
         df["Year"] = session.date.year
@@ -52,17 +35,13 @@ class CollectResults:
     def save_data(self,df:pd.DataFrame,year:int,gp:int,mode:str):
         filename = f"data/{year}_{gp:02}_{mode}.parquet"
         df.to_parquet(filename, index=False)
->>>>>>> 8641fe4 (Adicionado o envio dos dados para nuvem)
 
     def process(self,year, gp, mode):
         df = self.get_data(year, gp, mode)
         if df.empty:
             return False
         self.save_data(df,year,gp, mode)
-<<<<<<< HEAD
-=======
         time.sleep(1)
->>>>>>> 8641fe4 (Adicionado o envio dos dados para nuvem)
         return True
 
     def process_year_modes(self,year):
@@ -80,14 +59,6 @@ class CollectResults:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-<<<<<<< HEAD
-    parser.add_argument("--years","-y",nargs="+", type=int)
-    parser.add_argument("--modes","-m",nargs="+")
-
-    args = parser.parse_args()
-
-    collect = CollectResults(args.years,args.modes)
-=======
 
     parser.add_argument("--start", type=int, default=0)
     parser.add_argument("--stop", type=int, default=0)
@@ -100,6 +71,5 @@ if __name__ == "__main__":
     elif args.start !=0 and  args.stop !=0:
         years = [i for i in range(args.start, args.stop+1)]
         collect = CollectResults(years, args.modes)
->>>>>>> 8641fe4 (Adicionado o envio dos dados para nuvem)
     collect.process_years()
 # %%
